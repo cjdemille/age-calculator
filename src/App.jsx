65359220dd9f604303d26d0f9993453a26190fc7
cjdemille <cjdemille5@gmail.com>
2024-08-25 14:39:20 -0400

    prep to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
   const [dispMonth, setDispMonth] = useState("--")
   const [dispDay, setDispDay] = useState("--")
 
-
   
   const calculateAge = (dob) =>{
     dob = dob.toString();
