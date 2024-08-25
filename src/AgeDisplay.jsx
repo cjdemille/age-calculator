24dbd280fc8050b72fe152656bb9ae8f0f246433
cjdemille <cjdemille5@gmail.com>
2024-08-25 14:42:27 -0400

    fix merge error

--- src/AgeDisplay.jsx
+++ src/AgeDisplay.jsx
@@ -1,29 +1,12 @@
 import './AgeDisplay.css'
 
-<<<<<<< HEAD
+
 export default function AgeDisplay({dispYear, dispMonth, dispDay}) {
-    return (
+    return(
         <section className="AgeDisplay">
             <div className="DisplayLine"><span className="Result">{dispYear}</span> years</div>
             <div className="DisplayLine"><span className="Result">{dispMonth}</span> months</div>
             <div className="DisplayLine"><span className="Result">{dispDay}</span> days</div>
-=======
-<<<<<<< HEAD
-export default function AgeDisplay({days, months, years}) {
-    return (
-        <section className="AgeDisplay">
-            <div className="DisplayLine"><span className="Result">{years}</span> years</div>
-            <div className="DisplayLine"><span className="Result">{months}</span> months</div>
-            <div className="DisplayLine"><span className="Result">{days}</span> days</div>
-=======
-export default function AgeDisplay({dispYear, dispMonth, dispDay}) {
-    return (
-        <section className="AgeDisplay">
-            <div className="DisplayLine"><span className="Result">{dispYear}</span> years</div>
-            <div className="DisplayLine"><span className="Result">{dispMonth}</span> months</div>
-            <div className="DisplayLine"><span className="Result">{dispDay}</span> days</div>
->>>>>>> 6b0897f (create age calculator function)
->>>>>>> 9ceb9ed
         </section>
     )
 }
